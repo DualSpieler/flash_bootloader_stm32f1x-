7fa24cc41b523fff1b49b0544f076a08beeca849
Mahesh K <60285145+Mahesh-7@users.noreply.github.com>
2022-03-05 01:09:27 +0530

    Getting Started Tips and Faced issue comments added.

--- stubs/serial_interface/serial_interface/main.c
+++ stubs/serial_interface/serial_interface/main.c
@@ -6,7 +6,7 @@
 
 int main()
 {
-    //SI_Rx_Handler();
+    SI_Rx_Handler();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -19,3 +19,12 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//   7. In the Solution Explorer,go to soultion file >right click>Retarget project (to sync project file in local PC)
+//   8. In the Solution Explorer,go to soultion file >right click>Build.
+//   9. Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+
+//  issues faced:
+//  issue-1:during .sln file build
+//  if you got any build error like "unresolved external symbol SI_Rx_Handler referenced in function main"
+//  added and removed extern function declaration for SI_Rx_Handler in maini.c
+//  i guess this happends because of file func declaration not recognition issue in VS tool at first time
